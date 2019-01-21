4406142e4159a6db5ec1e7c5a46d2e5ebb25c390
hav-noms <clinton@havven.iogit config --global user.name hav-nomsgit config --global user.email clinton@havven.io>
2019-01-21 23:09:09 +1100

    code comments

--- contracts/Proxyable.sol
+++ contracts/Proxyable.sol
@@ -20,6 +20,11 @@ contract Proxyable is Owned {
      * optionalProxy modifiers, otherwise their invocations can use stale values. */ 
     address messageSender; 
 
+    /**
+     * @dev Constructor
+     * @param _proxy The proxy this contract exists behind
+     * @param _owner The account which controls this contract
+     */
     constructor(address _proxy, address _owner)
         Owned(_owner)
         public
@@ -28,6 +33,9 @@ contract Proxyable is Owned {
         emit ProxyUpdated(_proxy);
     }
 
+    /**
+     * @param _proxy The proxy this contract exists behind
+     */
     function setProxy(address _proxy)
         external
         onlyOwner
@@ -36,6 +44,10 @@ contract Proxyable is Owned {
         emit ProxyUpdated(_proxy);
     }
 
+    /**
+    * @dev We must get the msg.sender from the proxy otherwise the sender will be the Proxy
+    * @param sender The msg.sender or the caller of the function
+    */
     function setMessageSender(address sender)
         external
         onlyProxy
@@ -43,11 +55,21 @@ contract Proxyable is Owned {
         messageSender = sender;
     }
 
+    //-----------------------------------------------------------------
+    // Modifiers
+    //-----------------------------------------------------------------
+
+    /**
+    * @dev Only the proxy can call this function
+    */
     modifier onlyProxy {
         require(Proxy(msg.sender) == proxy, "Only the proxy can call this function");
         _;
     }
 
+    /**
+    * @dev This call could be called directly and not via the proxy
+    */
     modifier optionalProxy
     {
         if (Proxy(msg.sender) != proxy) {
@@ -56,6 +78,9 @@ contract Proxyable is Owned {
         _;
     }
 
+    /**
+    * @dev This call could be called directly and not via the proxy but must be the owner
+    */
     modifier optionalProxy_onlyOwner
     {
         if (Proxy(msg.sender) != proxy) {
