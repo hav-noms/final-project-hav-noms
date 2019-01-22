64152966846610c83855c98e5781d9524dae8e62
hav-noms <clinton@havven.iogit config --global user.name hav-nomsgit config --global user.email clinton@havven.io>
2019-01-22 20:47:43 +1100

    Truffle deploy and configuration

--- migrations/2_deploy_TokenExchange.js
+++ migrations/2_deploy_TokenExchange.js
@@ -6,8 +6,14 @@ const SafeDecimalMath = artifacts.require("SafeDecimalMath");
 const TokenExchange = artifacts.require("TokenExchange");
 const TokenExchangeState = artifacts.require("TokenExchangeState");
 
+// Update values before deployment
+const ZERO_ADDRESS = "0x" + "0".repeat(40);
+
+const SEED_ETHUSD_PRICE = "110";
+//const SEED_ETHUSD_PRICE = web3.utils.toWei("110"),
+
 module.exports = async function(deployer, network, accounts) {
-  const [deployerAccount, owner, oracle, feeAuthority, fundsWallet] = accounts;
+  const [deployerAccount] = accounts;
 
   // Note: This deployment script is not used on mainnet, it's only for testing deployments.
 
@@ -27,7 +33,7 @@ module.exports = async function(deployer, network, accounts) {
   // ----------------
   // TokenExchangeState
   // ----------------
-  console.log("Deploying TokenExchangeState...");
+  console.log("Deploying Storage...");
   // constructor(address _associatedContract)
   deployer.link(SafeDecimalMath, TokenExchangeState);
   const tokenExchangeState = await deployer.deploy(
@@ -39,28 +45,44 @@ module.exports = async function(deployer, network, accounts) {
   );
 
   // ----------------
-  // TokenExchange Proxy
+  // Proxy
   // ----------------
-  console.log("Deploying TokenExchange Proxy...");
+  console.log("Deploying Proxy...");
   const tokenExchangeProxy = await Proxy.new({ from: deployerAccount });
 
   // ----------------
   // TokenExchange
   // ----------------
   console.log("Deploying TokenExchange...");
-  // constructor(address payable _selfDestructBeneficiary,
-  // address payable _proxy,
-  // TokenExchangeState _externalState,
-  // uint _usdToEthPrice)
+  console.log("tokenExchangeProxy.address:", tokenExchangeProxy.address);
+  console.log("tokenExchangeState.address:", tokenExchangeState.address);
+  console.log("_usdToEthPrice:", SEED_ETHUSD_PRICE);
+  // constructor(address payable _selfDestructBeneficiary, address payable _proxy, TokenExchangeState _externalState, uint _usdToEthPrice)
   deployer.link(SafeDecimalMath, TokenExchange);
   const tokenExchange = await deployer.deploy(
+    TokenExchange,
     deployerAccount,
     tokenExchangeProxy.address,
     tokenExchangeState.address,
-
+    SEED_ETHUSD_PRICE,
     {
       from: deployerAccount,
-      gas: 8000000
+      gas: 5000000
     }
   );
+  console.log(" Successfully deployed all contracts:");
+
+  // ----------------
+  // TokenExchangeState
+  // ----------------
+  console.log("Configuring External State...");
+  tokenExchangeState.setAssociatedContract(tokenExchange.address);
+
+  // ----------------
+  // Proxy
+  // ----------------
+  console.log("Configuring Proxy...");
+  tokenExchangeProxy.setTarget(tokenExchange.address);
+
+  console.log(" Successfully Configured all contracts:");
 };
--- utils/testUtils.js
+++ utils/testUtils.js
@@ -2,3 +2,72 @@ const BN = require("bn.js");
 
 const ZERO_ADDRESS = "0x" + "0".repeat(40);
 const UNIT = web3.utils.toWei(new BN("1"), "ether");
+
+/**
+ *  Translates an amount to our cononical unit. We happen to use 10^18, which means we can
+ *  use the built in web3 method for convenience, but if unit ever changes in our contracts
+ *  we should be able to update the conversion factor here.
+ *  @param amount The amount you want to re-base to UNIT
+ */
+const toUnit = amount => web3.utils.toBN(web3.utils.toWei(amount, "ether"));
+const fromUnit = amount => web3.utils.fromWei(amount, "ether");
+
+/**
+ *  Gets the ETH balance for the account address
+ * 	@param account Ethereum wallet address
+ */
+const getEthBalance = account => web3.eth.getBalance(account);
+
+/**
+ *  Convenience method to assert that an event matches a shape
+ *  @param actualEventOrTransaction The transaction receipt, or event as returned in the event logs from web3
+ *  @param expectedEvent The event name you expect
+ *  @param expectedArgs The args you expect in object notation, e.g. { newOracle: '0x...', updatedAt: '...' }
+ */
+const assertEventEqual = (
+  actualEventOrTransaction,
+  expectedEvent,
+  expectedArgs
+) => {
+  // If they pass in a whole transaction we need to extract the first log, otherwise we already have what we need
+  const event = Array.isArray(actualEventOrTransaction.logs)
+    ? actualEventOrTransaction.logs[0]
+    : actualEventOrTransaction;
+
+  if (!event) {
+    assert.fail(new Error("No event was generated from this transaction"));
+  }
+
+  // Assert the names are the same.
+  assert.equal(event.event, expectedEvent);
+
+  assertDeepEqual(event.args, expectedArgs);
+  // Note: this means that if you don't assert args they'll pass regardless.
+  // Ensure you pass in all the args you need to assert on.
+};
+
+const assertRevert = async blockOrPromise => {
+  let errorCaught = false;
+  try {
+    const result =
+      typeof blockOrPromise === "function" ? blockOrPromise() : blockOrPromise;
+    await result;
+  } catch (error) {
+    assert.include(error.message, "revert");
+    errorCaught = true;
+  }
+
+  assert.equal(errorCaught, true, "Operation did not revert as expected");
+};
+
+module.exports = {
+  ZERO_ADDRESS,
+
+  toUnit,
+  fromUnit,
+
+  assertEventEqual,
+  assertRevert,
+
+  getEthBalance
+};
